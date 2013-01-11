6d2d47893162e441de21f4c4435d1be5462c8e54
Rafael Fiuza <rafael.fiuza@helabs.com.br>
2013-01-10 23:42:16 -0200

    removed template files

--- lib/send-scores.rb
+++ lib/send-scores.rb
@@ -1,12 +1,12 @@
-require "heroku-deploy/version"
+require "send-scores/version"
 
-module Heroku
-  module Deploy
+module Send
+  module Scores
   end
 end
 
 def gem_path
-  Gem::Specification.find_by_name('heroku-deploy').full_gem_path
+  Gem::Specification.find_by_name('send-scores').full_gem_path
 end
 
 Dir["#{gem_path}/lib/tasks/*.rake"].each { |ext| load ext } if defined?(Rake)
